e2a3381aec8816afa1cb73ff86a4b02c9bdb8fd9
SosukeKato <dokindokinpiano@gmail.com>
2025-12-13 18:04:36 +0900

    [update]前もってtransformを取得する処理を追加

--- Assets/_WorkSpace/Script/GameController.cs
+++ Assets/_WorkSpace/Script/GameController.cs
@@ -3,16 +3,30 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    Transform _tr;
+
     [SerializeField]
     GameObject _player;
     [SerializeField]
     GameObject[] _enemy;
+    [SerializeField]
+    GameObject _playerBullet;
+    [SerializeField]
+    GameObject[] _enemyBullet;
+    [SerializeField]
+    GameObject[] _stage;
     [SerializeField]
     Image _tileImage;
     [SerializeField]
     Image _gameOverImage;
     [SerializeField]
     Image _gameClearImage;
+
+    void Awake()
+    {
+        _tr = transform;
+    }
+
     void Start()
     {
         
