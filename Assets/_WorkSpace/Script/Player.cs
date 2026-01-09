eca93329aac3b4f45b54b6de5ab83567e3fd4bc5
SosukeKato <dokindokinpiano@gmail.com>
2026-01-09 16:39:28 +0900

    [update]Playerの移動範囲を制限

--- Assets/_WorkSpace/Script/Player.cs
+++ Assets/_WorkSpace/Script/Player.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Transform _tr;
+
+    Vector2 _playerPosition;
+    Vector2 _clampX;
+    Vector2 _clampY;
+    void Awake()
+    {
+        _tr = transform;
+    }
+
     void Start()
     {
         
@@ -9,6 +19,9 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        
+        _playerPosition.x = Mathf.Clamp(_playerPosition.x, _clampX.x, _clampX.y);
+        _playerPosition.y = Mathf.Clamp(_playerPosition.y, _clampY.x, _clampY.y);
+
+        _tr.position = new Vector2(_playerPosition.x, _playerPosition.y);
     }
 }
