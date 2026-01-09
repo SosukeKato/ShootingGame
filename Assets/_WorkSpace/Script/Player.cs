793f75f9465aeded205466336a16725b34700133
SosukeKato <dokindokinpiano@gmail.com>
2026-01-09 16:44:52 +0900

    [update]照準の移動範囲制限

--- Assets/_WorkSpace/Script/Player.cs
+++ Assets/_WorkSpace/Script/Player.cs
@@ -4,7 +4,11 @@ public class Player : MonoBehaviour
 {
     Transform _tr;
 
+    [SerializeField]
+    Transform _target;
+
     Vector2 _playerPosition;
+    Vector2 _targetPosition;
     Vector2 _clampX;
     Vector2 _clampY;
     void Awake()
@@ -21,7 +25,10 @@ public class Player : MonoBehaviour
     {
         _playerPosition.x = Mathf.Clamp(_playerPosition.x, _clampX.x, _clampX.y);
         _playerPosition.y = Mathf.Clamp(_playerPosition.y, _clampY.x, _clampY.y);
+        _targetPosition.x = Mathf.Clamp(_targetPosition.x, _clampX.x, _clampX.y);
+        _targetPosition.y = Mathf.Clamp(_targetPosition.y, _clampY.x, _clampY.y);
 
-        _tr.position = new Vector2(_playerPosition.x, _playerPosition.y);
+        _tr.position = _playerPosition;
+        _target.position = _targetPosition;
     }
 }
